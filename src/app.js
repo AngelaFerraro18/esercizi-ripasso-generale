2e37ef1cf56dea7b50e9e6971b5712225fabc8c3
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-09-09 14:49:45 +0200

    feat: ex from specialization

--- src/app.js
+++ src/app.js
@@ -98,3 +98,120 @@ console.log('i maggiorenni sono:', maggiorenni);
 const nomiMagg = maggiorenni.map(m => m.nome);
 const ordineNomi = [...nomiMagg].sort((a, b) => a.localeCompare(b));
 console.log(ordineNomi);
+
+
+
+
+const books = [
+    {
+        title: "React Billionaire",
+        pages: 250,
+        author: {
+            name: 'Alice',
+            age: 35
+        },
+        available: false,
+        price: '101€',
+        tags: ['advanced', 'js', 'react', 'senior']
+    },
+    {
+        title: "Advanced JS",
+        pages: 500,
+        author: {
+            name: 'Bob',
+            age: 20
+        },
+        available: true,
+        price: '25€',
+        tags: ['advanced', 'js', 'mid-senior']
+    },
+    {
+        title: "CSS Secrets",
+        pages: 320,
+        author: {
+            name: 'Alice',
+            age: 17
+        },
+        available: true,
+        price: '8€',
+        tags: ['html', 'css', 'junior']
+    },
+    {
+        title: "HTML Mastery",
+        pages: 200,
+        author: {
+            name: 'Charlie',
+            age: 50
+        },
+        available: false,
+        price: '48€',
+        tags: ['html', 'advanced', 'junior', 'mid-senior']
+    },
+];
+
+
+//snack 1 
+/*  Crea un array (longBooks) con i libri che hanno più di 300 pagine;
+Creare un array (longBooksTitles) che contiene solo i titoli dei libri contenuti in longBooks.
+Stampa in console ogni titolo nella console. */
+
+
+const longBooks = books.filter(b => b.pages > 300);
+console.log(longBooks);
+
+const longBooksTitles = longBooks.map(b => b.title);
+console.log(longBooksTitles);
+
+
+//snack 2
+/* Creare un array (availableBooks) che contiene tutti i libri disponibili.
+Crea un array (discountedBooks) con gli availableBooks, ciascuno con il prezzo scontato del 20% (mantieni lo stesso formato e arrotonda al centesimo)
+Salva in una variabile (fullPricedBook) il primo elemento di discountedBooks che ha un prezzo intero (senza centesimi). */
+
+const availableBooks = books.filter(b => b.available === true);
+console.log(availableBooks);
+
+const discountedBooks = availableBooks.map(b => {
+    const discount = (parseFloat(b.price) * 20) / 100;
+    const newPrice = (parseFloat(b.price)) - discount;
+
+    return {
+        ...b,
+        price: parseFloat(newPrice.toFixed(2))
+    }
+
+})
+
+console.log(discountedBooks);
+
+
+const fullPricedBook = discountedBooks.find(b => b.price % 1 === 0);
+console.log(fullPricedBook);
+
+
+//esercizio 3
+/* Creare un array (authors) che contiene gli autori dei libri.
+Crea una variabile booleana (areAuthorsAdults) per verificare se gli autori sono tutti maggiorenni.
+Ordina l’array authors in base all’età, senza creare un nuovo array.
+(se areAuthorsAdult è true, ordina in ordine crescente, altrimenti in ordine decrescente)*/
+
+const authors = books.map(b => b.author);
+console.log(authors);
+
+const areAuthorsAdults = authors.every(a => a.age >= 18);
+console.log('Sono tutti maggiorenni?', areAuthorsAdults);
+
+authors.sort((a, b) => b.age - a.age);
+
+
+//esercizio 4
+/* Creare un array (ages) che contiene le età degli autori dei libri.
+Calcola la somma delle età (agesSum) usando reduce.
+Stampa in console l’età media degli autori dei libri. */
+
+const ages = authors.map(a => a.age);
+
+const agesSum = ages.reduce((acc, curr) => acc + curr, 0);
+
+const averageAges = agesSum / authors.length;
+console.log(averageAges);
